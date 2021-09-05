bf3f7b971f52744133cb1984f73e2d01bea96cf1
Daniel Reichenpfader <d.reichenpfader@solgenium.com>
2021-09-05 13:18:47 +0200

    :sparkles: completes exercise1

---
+++ exercise1/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">{{ name }}</text>
+    <text-input
+      :style="{ height: 40, width: 100, borderColor: 'gray', borderWidth: 1 }"
+      v-model="text"
+    />
+    <image
+      v-if="imageShown"
+      :style="{ height: 150, width: 150 }"
+      :source="require('./img/me.jpg')"
+    />
+    <button :on-press="updateName" title="Update name" color="#841584" />
+    <button :on-press="toggleImage" title="Hide your face" />
+    <custom-button     
+      v-for="example in examples"
+      :key="example.text"
+      :item="example"
+    />
+  </view>
+</template>
+
+<script>
+import CustomButton from './components/CustomButton.vue'
+export default {
+  components: { CustomButton },
+  data() {
+    return {
+      name: 'Daniel',
+      text: '',
+      imageShown: true,
+      examples: [
+        { id: 1, text: 'foo' },
+        { id: 2, text: 'bar' },
+        { id: 3, text: 'foobar' },
+      ],
+    }
+  },
+  methods: {
+    updateName() {
+      this.name = this.text
+    },
+    toggleImage() {
+      this.imageShown = !this.imageShown
+    },
+  },
+}
+</script>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
---
+++ exercise1/components/CustomButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <touchable-opacity class="list-button">
+    <text>{{ item.id }}. {{ item.text }}</text>
+  </touchable-opacity>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      Type: Object,
+    },
+  },
+}
+</script>
+
+<style>
+.list-button {
+    
+  border-color: red;
+  border-style: solid;
+  border-radius: 5;
+  border-width: 2;
+  padding: 10px;
+}
+</style>
