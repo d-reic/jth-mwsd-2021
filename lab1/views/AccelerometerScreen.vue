ffabcf2acbc7e9365318cac516cecfb9cc9b9f5f
Daniel Reichenpfader <d.reichenpfader@solgenium.com>
2021-09-05 16:49:41 +0200

    :sparkles: adds GPS data and prepares acceleration

--- lab1/views/AccelerometerScreen.vue
+++ lab1/views/AccelerometerScreen.vue
@@ -1,24 +1,42 @@
 <template>
   <view class="container">
     <text class="text-color-primary">Accelerometer</text>
-   <button title="Go to menu screen" @press="goToMenuScreen"></button>
+    <text class="text-color-primary">{{ accelerometerData.x }}</text>
+    <text class="text-color-primary">{{ accelerometerData.y }}</text>
+    <text class="text-color-primary">{{ accelerometerData.z }}</text>
+    <button title="Go to menu screen" @press="goToMenuScreen"></button>
   </view>
 </template>
 
 <script>
+import { Accelerometer } from 'expo-sensors'
+
+Accelerometer.setUpdateInterval(1000);
+Accelerometer.addListener(accelerometerData => {
+  this.accelerometerData = accelerometerData;
+});
+
 export default {
   // Declare `navigation` as a prop
   props: {
     navigation: {
-      type: Object
+      type: Object,
+    },
+  },
+  data() {
+    return {
+      accelerometerData: {},
     }
   },
   methods: {
     goToMenuScreen() {
-      this.navigation.navigate("Menu");
-    }
-  }
+      this.navigation.navigate('Menu')
+    },
+  },
 }
+
+
+
 </script>
 
 <style>
--- lab1/views/GPSScreen.vue
+++ lab1/views/GPSScreen.vue
@@ -1,23 +1,60 @@
 <template>
   <view class="container">
     <text class="text-color-primary">GPS</text>
-   <button title="Go to menu screen" @press="goToMenuScreen"></button>
+    <touchable-opacity :on-press="getLocation">
+      <text class="text-field-title">Get Location</text>
+    </touchable-opacity>
+    <text class="text-field-title">Location Object:</text>
+    <text>{{ location }}</text>
+    <text class="text-field-title">Latitude Only:</text>
+    <text>{{ latitude }}</text>
+
+    <text class="text-error">{{ errorMessage }}</text>
+    <button title="Go to menu screen" @press="goToMenuScreen"></button>
   </view>
 </template>
 
 <script>
+
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
+
 export default {
   // Declare `navigation` as a prop
   props: {
     navigation: {
-      type: Object
+      type: Object,
+    },
+  },
+  data: function () {
+    return {
+      location: {},
+      latitude: '',
+      errorMessage: '',
     }
   },
   methods: {
     goToMenuScreen() {
-      this.navigation.navigate("Menu");
-    }
-  }
+      this.navigation.navigate('Menu')
+    },
+    getLocation: function () {
+      Permissions.askAsync(Permissions.LOCATION)
+        .then((status) => {
+          if (!status.granted) {
+            this.errorMessage = 'Permission to access location was denied'
+          } else if (status.granted) {
+            Location.getCurrentPositionAsync({}).then((location) => {
+              this.location = location
+              this.latitude = location.coords.latitude
+              this.errorMessage = ''
+            })
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
+  },
 }
 </script>
 
