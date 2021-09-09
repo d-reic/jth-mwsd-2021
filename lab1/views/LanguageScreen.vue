e135bc0b22da528ed8e5a3669c297a9c75addee8
Daniel Reichenpfader <d.reichenpfader@solgenium.com>
2021-09-09 14:05:20 +0200

    :sparkles: completes language task

--- lab1/views/LanguageScreen.vue
+++ lab1/views/LanguageScreen.vue
@@ -1,11 +1,15 @@
 <template>
   <view class="container">
     <text class="text-color-primary">Language</text>
+    <text class="text-color-primary">Configured language: {{ language }}</text>
+    <text class="text-color-primary" v-if="language == 'en_US'">This is an English text!</text>
+     <text class="text-color-primary" v-if="language == 'de_DE'">Das ist ein Text in deutscher Sprache!</text>
     <button title="Go to menu screen" @press="goToMenuScreen"></button>
   </view>
 </template>
 
 <script>
+import { NativeModules, Platform } from 'react-native'
 export default {
   // Declare `navigation` as a prop
   props: {
@@ -15,8 +19,15 @@ export default {
   },
   data() {
     return {
-      accelerometerData: {},
+     language: '',
     }
+  },
+  mounted: function () {
+    this.language =
+      Platform.OS === 'ios'
+        ? NativeModules.SettingsManager.settings.AppleLocale ||
+          NativeModules.SettingsManager.settings.AppleLanguages[0] // iOS 13
+        : NativeModules.I18nManager.localeIdentifier    
   },
   methods: {
     goToMenuScreen() {
