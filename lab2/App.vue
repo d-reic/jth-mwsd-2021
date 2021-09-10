c06293f61f37a6d66df16ece547513f67eb881ae
Daniel Reichenpfader <d.reichenpfader@solgenium.com>
2021-09-10 13:26:20 +0200

    :sparkles: start lab2

---
+++ lab2/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <view class="container">
+    <app-navigator></app-navigator>
+    <button :title="currentPage" @press="navigate"></button>
+  </view>
+</template>
+
+<script>
+import { createAppContainer, createStackNavigator } from 'vue-native-router'
+
+import SignUpComponent from './views/SignUpComponent.vue'
+import SignInComponent from './views/SignInComponent.vue'
+import ChatComponent from './views/ChatComponent.vue'
+
+const StackNavigator = createStackNavigator(
+  {
+    SignUp: SignUpComponent,
+    SignIn: SignInComponent,
+    Chat: ChatComponent,
+  },
+  {
+    initialRouteName: 'SignIn',
+  },
+)
+
+const AppNavigator = createAppContainer(StackNavigator)
+
+export default {
+  components: { AppNavigator },
+  data() {
+    return {
+      currentPage: 'Test',
+    }
+  },
+  methods: {
+    navigate() {
+      console.log('Navigate')
+      //this.navigation.navigate('Menu')
+    },
+  },
+}
+</script>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
